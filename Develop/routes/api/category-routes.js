d593652b304ef57d75c0454e1d1b9d43c19c7034
Gianfranco De Vettori <Ako@Akos-Mac-2.local>
2024-01-31 20:33:58 -0700

    added routes to category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,51 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: Product
+  })
+  .then(results => {
+    res.json(results)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id, {
+    include: Product
+  })
+  .then(result => {
+    res.json(result)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then(result => {
+    res.json(result)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {id: req.params.id}
+  })
+  .then(result => {
+    res.json(result)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {id: req.params.id}
+  })
+  .then(result => {
+    res.json(result)
+  })
 });
 
 module.exports = router;
